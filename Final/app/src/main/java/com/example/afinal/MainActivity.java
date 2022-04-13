db99d238b21fe1b15b5ee3dd40e2d42816501643
Daitori <100677113+Daitori@users.noreply.github.com>
2022-04-13 11:06:55 +0200

    Upload Initial
    Functionnalite present SMS envoie
    Interface pour contact et Info user

---
+++ Final/app/src/main/java/com/example/afinal/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.afinal;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.afinal.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_REQUEST_CODE_SEND_SMS = 1;
+    private static final String LOG_TAG = "AndroidExample";
+    public EditText editTextPhoneNumber;
+    public EditText editTextMessage;
+
+    /*public class patient{
+        public String Prenom;
+        public String Nom;
+        public Date Naissance;
+        public int NumeroSocial;
+        public String Condition;
+    }
+    public class messageenvoie{
+        public String Message;
+        public int Telephone;
+
+    }*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.example.afinal.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void SaveSpinner(){}
+
+
+    public  void askPermissionAndSendSMS() {
+        int sendSmsPermisson = ActivityCompat.checkSelfPermission(this ,Manifest.permission.SEND_SMS);
+        if (sendSmsPermisson != PackageManager.PERMISSION_GRANTED) {
+            // If don't have permission so prompt the user.
+            this.requestPermissions(new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSION_REQUEST_CODE_SEND_SMS
+            );
+            return;
+        }
+        this.sendSMS_by_smsManager();
+
+    }
+
+    private void sendSMS_by_smsManager()  {
+        this.editTextPhoneNumber = this.findViewById(R.id.Contact);
+        this.editTextMessage = this.findViewById(R.id.Message);
+        String phoneNumber = this.editTextPhoneNumber.getText().toString();
+        String message = this.editTextMessage.getText().toString();
+
+        try {
+            // Get the default instance of the SmsManager
+            SmsManager smsManager = SmsManager.getDefault();
+            // Send Message
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            Log.i( LOG_TAG,"Your sms has successfully sent!");
+            Toast.makeText(getApplicationContext(),"Your sms has successfully sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Log.e( LOG_TAG,"Your sms has failed...", ex);
+            Toast.makeText(getApplicationContext(),"Your sms has failed... " + ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        if (requestCode == MY_PERMISSION_REQUEST_CODE_SEND_SMS) {// Note: If request is cancelled, the result arrays are empty.
+            // Permissions granted (SEND_SMS).
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Log.i(LOG_TAG, "Permission granted!");
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+                this.sendSMS_by_smsManager();
+            }
+            // Cancelled or denied.
+            else {
+                Log.i(LOG_TAG, "Permission denied!");
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // When results returned
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE_SEND_SMS) {
+            if (resultCode == RESULT_OK) {
+                // Do something with data (Result returned).
+                Toast.makeText(this, "Action OK", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Action canceled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Action Failed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ Final/app/src/main/java/com/example/afinal/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,48 @@
+package com.example.afinal.ui.notifications;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.afinal.MainActivity;
+import com.example.afinal.R;
+import com.example.afinal.databinding.FragmentNotificationsBinding;
+
+import java.util.ArrayList;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Spinner spinner = requireView().findViewById(R.id.spinner);
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Default");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arrayList1);  // layout for spinner itself
+        dataAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item); // layout for spinner dropdown items
+        spinner.setAdapter(dataAdapter);
+        Button buttonSend = requireView().findViewById(R.id.button_first4);
+        buttonSend.setOnClickListener(v -> ((MainActivity) requireActivity()).askPermissionAndSendSMS());
+        Button buttonSave = requireView().findViewById(R.id.button_first2);
+        buttonSave.setOnClickListener(v -> ((MainActivity) requireActivity()).SaveSpinner());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
